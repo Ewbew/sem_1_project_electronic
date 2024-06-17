e6aac0bce38a185851cb3479a05da475a664ce5f
Audrius <asa@exigo.dk>
2024-06-17 12:09:03 +0200

    Driving control improvements
    Simplified the if statements

--- projekt_bil_software/DrivingControl.cpp
+++ projekt_bil_software/DrivingControl.cpp
@@ -7,70 +7,29 @@
 
 #include "DrivingControl.h"
 
+const uint8_t SPEED_LEVELS[] = {100, 90, 65, 50, 90, 50, 50, 50, 80, 80, 50, 0};
+const uint8_t MAX_COUNTER = sizeof(SPEED_LEVELS) / sizeof(SPEED_LEVELS[0]);
+
 DrivingControl::DrivingControl(int* counter_pointer):counter_pointer_(counter_pointer){}
 
 
 // OBS: Disse værdier skal ændres indtil at bilen kører igennem uden for mange fejl; de bestemmes gennem testkørlser.
 // OBS: Det kan være, at bilen bliver nødt til at få sat speed til 0, i stedet for bare at skifte retning.
-int DrivingControl::get_speed(){
-	if(*counter_pointer_==0){
-		return 100;
-	} else if(*counter_pointer_==1){
-		return 90;
-	} else if(*counter_pointer_==2){
-		return 65;
-	} else if(*counter_pointer_==3){
-		return 50;
-	} else if(*counter_pointer_==4){
-		return 90;
-	
-	// Her skal vi være opmærksomme på, at bilen skal standes efter at have passeret refleksbrik #6, og så derefter køre baglæns-
-	// det skal evt. implementeres i get
-	} else if (*counter_pointer_==5){
-		return 50;
-	} else if (*counter_pointer_==6){
-		// Baglæns
-		return 50;
-	} else if (*counter_pointer_==7){
-		// Baglæns
-		return 50;
-	} else if (*counter_pointer_==8){
-		// Forlæns igen
-		return 80;
-	} else if (*counter_pointer_==9){
-		return 80;
-	} else if(*counter_pointer_==10){
-		return 50;
-	} else if(*counter_pointer_==11){
-		// Slutposition
-		return 0;
-	} else {
-		// Error hvis andet
+int DrivingControl::get_speed() {
+	if (*counter_pointer_ >= 0 && *counter_pointer_ < MAX_COUNTER) {
+		return SPEED_LEVELS[*counter_pointer_];
+		} else {
 		return -1;
 	}
 }
 
-bool DrivingControl::is_forward_direction(){
-	if(*counter_pointer_<6){
-		return true;
-	} else if(*counter_pointer_<8 ){
-		return false;
-	} else {
-		return true;
-	}
+bool DrivingControl::is_forward_direction() {
+	return *counter_pointer_ < 6 || *counter_pointer_ >= 8;
 }
 
 // if-statements følger samme logik som get_speed()
-bool DrivingControl::get_lights_state(){
-	if(*counter_pointer_<6){
-		return true;
-	} else if (*counter_pointer_==6){
-		return false;
-	} else if(*counter_pointer_<11){
-		return true;
-	} else {
-		return false;
-	}
+bool DrivingControl::get_lights_state() {
+	return *counter_pointer_ < 6 || (*counter_pointer_ >= 7 && *counter_pointer_ < 11);
 }
 
 bool DrivingControl::get_brake_state(){
@@ -80,12 +39,7 @@ bool DrivingControl::get_brake_state(){
 	(*counter_pointer_)++;
 	// Hvis nuværende fart end lavere end tidligere fart, skal der returneres true;
 	// Derudover, hvis der er skiftet retning siden sidste refleksbrik, må bilen også skulle bremse/standse
-	if (get_speed() < previous_speed || is_forward_direction() != previous_direction){
-		return true;
-	} else {
-		return false;
-	}
-		
+    return get_speed() < previous_speed || is_forward_direction() != previous_direction;
 	// Man kan specificere at den bremser ud fra counter_pointer
 	// Ellers kan man have en privat variabel, der husker tidligere speed, 
 	// og ud fra den og nuværende speed afgøre, om der bliver bremset eller ej.
